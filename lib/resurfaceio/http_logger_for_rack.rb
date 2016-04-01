d9bc6a7dd9ac2bd829db4363f1f3513789fa1dae
Rob Dickinson <robfromboulder@gmail.com>
2016-03-31 21:42:58 -0600

    Tweaks and test cases for rack logger

--- lib/resurfaceio/http_logger_for_rack.rb
+++ lib/resurfaceio/http_logger_for_rack.rb
@@ -4,7 +4,7 @@
 require 'rack'
 require 'resurfaceio/http_logger_factory'
 
-class HttpLoggerForRack
+class HttpLoggerForRack # http://rack.rubyforge.org/doc/SPEC.html
 
   def initialize(app)
     @app = app
@@ -15,11 +15,18 @@ class HttpLoggerForRack
     if status != 304
       response = Rack::Response.new(body, status, headers)
       content_type = response.content_type
-      is_html = content_type.present? && (content_type.downcase.index('text/html') == 0)
-      if is_html
-        request = Rack::Request.new(env)
-        HttpLoggerFactory.get.log_request(request)
-        HttpLoggerFactory.get.log_response(response)
+      unless content_type.nil?
+        content_type = content_type.downcase
+        is_html = content_type.include?('text/html')
+        is_json = content_type.include?('application/json')
+        is_soap = content_type.include?('application/soap+xml')
+        is_xml1 = content_type.include?('application/xml')
+        is_xml2 = content_type.include?('text/xml')
+        if is_html || is_json || is_soap || is_xml1 || is_xml2
+          request = Rack::Request.new(env)
+          HttpLoggerFactory.get.log_request(request)
+          HttpLoggerFactory.get.log_response(response)
+        end
       end
     end
     [status, headers, body]
--- spec/resurfaceio/http_logger_for_rack_spec.rb
+++ spec/resurfaceio/http_logger_for_rack_spec.rb
@@ -2,6 +2,7 @@
 # Copyright (c) 2016 Resurface Labs LLC, All Rights Reserved
 
 require 'resurfaceio/logger'
+require_relative 'mocks'
 
 describe HttpLoggerForRack do
 
@@ -9,6 +10,38 @@ describe HttpLoggerForRack do
     expect(HttpLoggerForRack.class.equal?(Resurfaceio::HttpLoggerForRack.class)).to be true
   end
 
-  # todo missing test cases
+  it 'logs rack calls' do
+    logger = HttpLoggerFactory.get.disable.tracing_start
+    begin
+      filter = HttpLoggerForRack.new(MockHtmlApp.new)
+      filter.call(MOCK_ENV)
+      expect(logger.tracing_history.length).to eql(2) # todo check tracing history
+
+      filter = HttpLoggerForRack.new(MockJsonApp.new)
+      filter.call(MOCK_ENV)
+      expect(logger.tracing_history.length).to eql(4) # todo check tracing history
+    ensure
+      logger.tracing_stop.enable
+    end
+  end
+
+  it 'skips logging for redirects and unmatched content types' do
+    logger = HttpLoggerFactory.get.disable.tracing_start
+    begin
+      filter = HttpLoggerForRack.new(MockCustomApp.new)
+      filter.call(MOCK_ENV)
+      expect(logger.tracing_history.length).to eql(0)
+
+      filter = HttpLoggerForRack.new(MockCustomRedirectingApp.new)
+      filter.call(MOCK_ENV)
+      expect(logger.tracing_history.length).to eql(0)
+
+      filter = HttpLoggerForRack.new(MockHtmlRedirectingApp.new)
+      filter.call(MOCK_ENV)
+      expect(logger.tracing_history.length).to eql(0)
+    ensure
+      logger.tracing_stop.enable
+    end
+  end
 
 end
