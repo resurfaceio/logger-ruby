3b6753328cec1b4b2681fe7e4abcc08c50ac2fcc
Rob Dickinson <robfromboulder@gmail.com>
2016-04-24 23:17:52 -0600

    Update version to 1.2.0

--- spec/resurfaceio/http_logger_spec.rb
+++ spec/resurfaceio/http_logger_spec.rb
@@ -138,7 +138,7 @@ describe HttpLogger do
     expect(version).to be_kind_of String
     expect(version).not_to be nil
     expect(version.length).to be > 0
-    expect(version.start_with?('1.1.')).to be true
+    expect(version.start_with?('1.2.')).to be true
     expect(version.include?('\\')).to be false
     expect(version.include?('\"')).to be false
     expect(version.include?('\'')).to be false
