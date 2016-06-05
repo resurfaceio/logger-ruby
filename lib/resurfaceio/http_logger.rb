980cdf0836fded2b12180a9976e39f1d2f4c14e9
Rob Dickinson <robfromboulder@gmail.com>
2016-06-04 22:14:46 -0600

    #4 Clean up header logic

--- lib/resurfaceio/http_logger.rb
+++ lib/resurfaceio/http_logger.rb
@@ -21,62 +21,16 @@ class HttpLogger < UsageLogger
     JsonMessage.start(json, 'http_request', agent, version, now) << ','
     JsonMessage.append(json, 'method', request.request_method) << ','
     JsonMessage.append(json, 'url', request.url) << ','
-
-    # add headers to json
-    JsonMessage.append(json, 'headers') << ':['
-    respond_to_env = request.respond_to?(:env)
-    respond_to_headers = request.respond_to?(:headers)
-    if respond_to_env || respond_to_headers
-      first = true
-      headers = respond_to_env ? request.env : request.headers
-      headers.each do |name, value|
-        if name =~ /^CONTENT_TYPE/
-          JsonMessage.append(json << (first ? '{' : ',{'), 'content-type', value) << '}'
-          first = false
-        end
-        if name =~ /^HTTP_/
-          JsonMessage.append(json << (first ? '{' : ',{'), name[5..-1].downcase.tr('_', '-'), value) << '}'
-          first = false
-        end
-      end
-    end
-    json << ']'
-
-    # add body to json
-    unless body.nil? && request.body.nil?
-      json << ','
-      JsonMessage.append(json, 'body', body.nil? ? request.body : body)
-    end
+    append_request_headers(json, request)
+    JsonMessage.append(json << ',', 'body', body.nil? ? request.body : body) unless body.nil? && request.body.nil?
     JsonMessage.stop(json)
   end
 
   def format_response(json, now, response, body=nil)
     JsonMessage.start(json, 'http_response', agent, version, now) << ','
     JsonMessage.append(json, 'code', response.status) << ','
-
-    # add headers to json
-    first = true
-    found_content_type = false
-    JsonMessage.append(json, 'headers') << ':['
-    if response.respond_to?(:headers)
-      response.headers.each do |name, value|
-        name = name.downcase
-        found_content_type = true if name =~ /^content\-type/
-        JsonMessage.append(json << (first ? '{' : ',{'), name, value) << '}'
-        first = false
-      end
-    end
-    unless found_content_type || response.content_type.nil?
-      # for whatever reason, content_type is present but isn't reflected by headers
-      JsonMessage.append(json << (first ? '{' : ',{'), 'content-type-MISSINGHEADER', response.content_type) << '}'
-    end
-    json << ']'
-
-    # add body to json
-    unless body.nil? && response.body.nil?
-      json << ','
-      JsonMessage.append(json, 'body', body.nil? ? response.body : body)
-    end
+    append_response_headers(json, response)
+    JsonMessage.append(json << ',', 'body', body.nil? ? response.body : body) unless body.nil? && response.body.nil?
     JsonMessage.stop(json)
   end
 
@@ -102,6 +56,46 @@ class HttpLogger < UsageLogger
     else
       true
     end
+  end
+
+  protected
+
+  def append_request_headers(json, request)
+    JsonMessage.append(json, 'headers') << ':['
+    first = true
+    respond_to_env = request.respond_to?(:env)
+    if respond_to_env || request.respond_to?(:headers)
+      headers = respond_to_env ? request.env : request.headers
+      headers.each do |name, value|
+        if name =~ /^CONTENT_TYPE/
+          JsonMessage.append(json << (first ? '{' : ',{'), 'content-type', value) << '}'
+          first = false
+        end
+        if name =~ /^HTTP_/
+          JsonMessage.append(json << (first ? '{' : ',{'), name[5..-1].downcase.tr('_', '-'), value) << '}'
+          first = false
+        end
+      end
+    end
+    json << ']'
+  end
+
+  def append_response_headers(json, response)
+    JsonMessage.append(json, 'headers') << ':['
+    first = true
+    found_content_type = false
+    if response.respond_to?(:headers)
+      response.headers.each do |name, value|
+        name = name.downcase
+        found_content_type = true if name =~ /^content\-type/
+        JsonMessage.append(json << (first ? '{' : ',{'), name, value) << '}'
+        first = false
+      end
+    end
+    unless found_content_type || response.content_type.nil?
+      JsonMessage.append(json << (first ? '{' : ',{'), 'content-type', response.content_type) << '}'
+    end
+    json << ']'
   end
 
 end
